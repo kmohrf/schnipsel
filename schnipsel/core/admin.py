4e8a86d3da7d05ac658b0b28e7f50cfc61daccec
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2020-04-04 20:01:29 +0200

    fix user admin
    With the changes from 7756dedd33cdf0b53fc1870f6e1632783a773fe5 (drop
    usernames in favor of emails) our user model was no longer compatible
    with the default django admin for users.

--- schnipsel/core/admin.py
+++ schnipsel/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
@@ -39,6 +40,25 @@ class BoardAdmin(admin.ModelAdmin):
 @admin.register(models.User)
 class UserAdmin(UserAdmin):
     ordering = ("email",)
-    fieldsets = UserAdmin.fieldsets + (
-        ("Custom Data", {"fields": ("name", "language", "avatar",)}),
+    list_display = ("email", "name", "is_staff")
+    search_fields = ("name", "email")
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("name", "language", "avatar")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
